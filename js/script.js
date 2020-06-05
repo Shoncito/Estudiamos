2bf7ae15a52c9a86090b155875e76806f78a28a8
Jessiesaurus <jessicarendon318@gmail.com>
2020-06-05 11:29:39 -0500

    Ajustes

--- js/script.js
+++ js/script.js
@@ -34,3 +34,30 @@ $(".CategoriaSnack").click(function(){
 	$(this).addClass("brillo");
 });
 
+$('.special.cards .image').dimmer({
+  on: 'hover'
+});
+
+var valor=0;
+function carrito(boton){
+    var contador = document.getElementById("contador").value;
+    if (boton.value=='aumentar') {
+    	if (valor<30) {
+    		valor++
+    	}
+    }else{
+    	if (valor>0) {
+    		valor--
+    	}
+    }
+    document.getElementById("contador").textContent = valor;
+}
+
+var nombreProducto=["CocaCola", "Pepsi", "H2O", "Té", "Café"];
+var precioProducto=["12.000", "5.000", "6.000", "3.000", "1.000"];
+var imagenProducto=["img/estudio.jpg", "img/estudio.jpg", "img/estudio.jpg", "img/estudio.jpg", "img/estudio.jpg"];
+
+ var PRODUCTO = document.getElementsByName("PRODUCTO");
+ for (i in nombreProducto){
+ PRODUCTO[i].innerHTML = '<div class="content"><div name="NOMBREPRODUCTO" class="header">'+nombreProducto[i]+' </div></div><img src="' +imagenProducto[i]+ '"><div name="PRECIOPRODUCTO" class="meta">' +precioProducto[i]+ '</div><div class="extra content ui large buttons"><button class="ui brown button" id="disminuir" onclick="carrito(this)" value="disminuir">-</button><div id="p"><p id="contador" value=""></p></div><button class="ui orange button" id="aumentar" onclick="carrito(this)" value="aumentar">+</button></div>';
+ }
