e6856d0a453c488e5286ac6ff9a6eb74de6df07b
unknown <santipego0001@gmail.com>
2020-06-06 10:34:49 -0500

    Se agregan notificaciones de pomodoro y tutorias

--- js/notificaciones.js
+++ js/notificaciones.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
-	Push.create("Hora de estudiar",{
-		body:"No olvides tus prioridades!"
-	});
+    if(!Notification){
+        alert("Las notificaciones no están habilitadas para este navegador");
+        return;
+    }else if(Notification.permission!=="granted"){
+        Notification.requestPermission();
+    }
 });
 
+/**
+ * Función que notifica
+ * @param {String} mensaje 
+ */
+function notificar(subtipo, mensaje){
+    if(Notification.permission!=="granted"){
+        Notification.requestPermission();
+    }else{
+        var notificacion = new Notification(subtipo,{
+            icon: "img/estudio.jpg",
+            body: mensaje,
+            timestamp: 800000
+        })
+    }
+    
+}
