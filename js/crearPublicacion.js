93b15f73fbf076ed0e780e9d081820559ac1e1a0
Jessiesaurus <jessicarendon318@gmail.com>
2020-06-10 20:35:28 -0500

    Crear publicación

---
+++ js/crearPublicacion.js
@@ -0,0 +1,13 @@
+  $("#pub").click(function(evt){
+    evt.preventDefault();
+    var titulo = $("#titulop").val();
+    var contenido = $("#contp").val();
+    var obj={
+        tipo: "crear publicación",
+        idUsuario: usuario.usuario,
+        titulo: titulo,
+        contenido: contenido,
+        idMateria: idMateria,
+    }
+    enviarMensaje(obj);
+});
